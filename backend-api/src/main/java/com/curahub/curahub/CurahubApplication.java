43ea85d7dcd6f20b738d24a35c9a952f2b091831
twinklymeat <twinklymeat8990@gmail.com>
2025-10-23 00:34:06 -0400

    added base java project

---
+++ backend-api/src/main/java/com/curahub/curahub/CurahubApplication.java
@@ -0,0 +1,13 @@
+package com.curahub.curahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurahubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurahubApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/com/curahub/curahub/CurahubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curahub.curahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurahubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
