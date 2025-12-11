86e2d1d75220e926b69a954004209435800ae3c6
twinklymeat <twinklymeat8990@gmail.com>
2025-12-11 10:58:51 -0500

    everything works rn

--- backend-api/src/main/java/com/curahub/curahub/reviews/ReviewsController.java
+++ backend-api/src/main/java/com/curahub/curahub/reviews/ReviewsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,6 +20,11 @@ public class ReviewsController {
     @GetMapping("/{id}")
     public ResponseEntity<Object> getReviewsByDoctor(@PathVariable long id) {
         return ResponseEntity.ok(reviewsService.getReviewsByDoctor(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> addUserReview(@RequestBody Reviews review) {
+        return ResponseEntity.ok(reviewsService.addReview(review));
     }
 
     @PostMapping("/doctor/add")
